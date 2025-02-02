4524bd8d87ed8f1b572cbcb2ae60f8fedb3625a2
zyt0s <jannikpalma@gmail.com>
2025-02-02 21:15:41 +0100

    4

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ window.onload=function(){
     const neinButton = document.getElementById("nein");
     const page = document.getElementById("page");
     const page2 = document.getElementById("page2");
-    page2.style.display="none";
+    
 
     jaButton.addEventListener("click", function() {
         page.style.display="none";
