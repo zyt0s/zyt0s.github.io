d845f73e9871996059a367fbd713993d4342c639
zyt0s <jannikpalma@gmail.com>
2025-02-02 21:02:28 +0100

    2

---
+++ script.js
@@ -0,0 +1,10 @@
+
+window.onload=function(){
+    const jaButton = document.getElementById("ja");
+    const neinButton = document.getElementById("nein");
+    const page = document.getElementById("page");
+
+    jaButton.addEventListener("click", function() {
+        page.style.display="none";
+     });
+     }
